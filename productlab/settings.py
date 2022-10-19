f2c48e23136a36ad966f293726ebf5af143cfcbc
Darya Stasevich <stasevich.daria@gmail.com>
2022-10-19 16:59:26 +0300

    added db postgres, updated requirements.txt

--- productlab/settings.py
+++ productlab/settings.py
@@ -80,12 +80,19 @@ WSGI_APPLICATION = 'productlab.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'Nikol2005',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
